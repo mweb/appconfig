9652ae7078101ba07943d6332c50f17b6e1e757f
Mathias Weber <mathew.weber@gmail.com>
2013-06-19 22:12:11 +0200

    fixed packages data to get a valid python package

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = [ 'appdirs',
+requires = ['appdirs',
         ]
-tests_requires = [ 'nose',
+tests_requires = ['nose',
         ]
 
 setup(name='appconfig',
@@ -17,13 +17,14 @@ setup(name='appconfig',
         description='An easy to use config file wrapper.',
         long_description=README + '\n\n' + CHANGES,
         classifiers=[
+            "Development Status :: 4 - Beta",
             "Programming Language :: Python",
             "Topic :: Software Development :: Libraries :: Python Modules",
             ],
         author='Mathias Weber',
         author_email='mathew.weber@gmail.com',
         license='BSD',
-        url='',
+        url='http://mweb.github.io/appconfig',
         test_suite='nose.collector',
         packages=find_packages(),
         install_requires=requires,
